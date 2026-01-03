d4293b4b379bdf7cfdfb344cdd2799e72c8d740b
01shrvan <seemabenke12@gmail.com>
2026-01-03 21:02:25 +0530

    chore: remove unused assets and deprecated codebase

--- src/sanity/schema/index.ts
+++
@@ -1,6 +0,0 @@
-import { archiveGFXType } from "./archiveGFXType";
-import { archivePhotoType } from "./archivePhotoType";
-import { projectType } from "./projectType";
-import { writingType } from "./writingType";
-
-export const schemaTypes = [projectType, writingType, archiveGFXType, archivePhotoType];
